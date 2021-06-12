72cd7cfa750e94078d1e16b9e914445f49180a6d
kjeong0 <kjeong0@gmail.com>
2021-06-11 21:27:01 -0400

    Add hazards

--- Assets/_/Scripts/PlayerScripts/ControllablePlayer.cs
+++ Assets/_/Scripts/PlayerScripts/ControllablePlayer.cs
@@ -5,5 +5,14 @@ namespace Assets.__.Scripts.PlayerScripts
     public class ControllablePlayer : MonoBehaviour
     {
         public int PlayerIndex;
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            //Check for a match with the specified name on any GameObject that collides with your GameObject
+            if (collision.gameObject.tag == "Hazard")
+            {
+                SceneHandler.RestartCurrentScene();
+            }
+        }
     }
 }
