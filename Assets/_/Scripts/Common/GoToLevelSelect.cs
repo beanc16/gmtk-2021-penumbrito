bc3960c51b59dc4d825e0ff41a524c79e0e397ef
Silevran <Meliroon@gmail.com>
2021-06-12 16:49:28 -0700

    Some sound control polish.
    Adding some help to level 5
    Game Over has a way to go to Level Select

---
+++ Assets/_/Scripts/Common/GoToLevelSelect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class GoToLevelSelect : MonoBehaviour
+{
+    private Button button;
+
+    private void Start()
+    {
+        this.button = this.GetComponent<Button>();
+        this.button.onClick.AddListener(this.OnMainMenu);
+    }
+
+    private void OnDestroy()
+    {
+        this.button.onClick.RemoveAllListeners();
+    }
+
+    private void OnMainMenu()
+    {
+        SceneHandler.LoadScene("LevelSelect");
+    }
+}
