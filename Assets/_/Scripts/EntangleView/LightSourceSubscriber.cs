ddbbbda3735e2fb49c4498aaab96200e1fcee5ed
Silevran <Meliroon@gmail.com>
2021-06-11 18:55:03 -0700

    Lighting in the scene for active vs inactive.

---
+++ Assets/_/Scripts/EntangleView/LightSourceSubscriber.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace Assets.__.Scripts.EntangleView
+{
+    public class LightSourceSubscriber : MonoBehaviour
+    {
+        [SerializeField] private int index;
+        [SerializeField] private bool active;
+
+        private void Awake()
+        {
+            if (active)
+            {
+                GameModel.GetInstance().IndexToLight.Add(index, this.GetComponent<Light2D>());
+            }
+            else
+            {
+                GameModel.GetInstance().IndexToDark.Add(index, this.GetComponent<Light2D>());
+            }
+        }
+    }
+}
