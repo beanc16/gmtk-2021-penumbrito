8b8a9fec043038ac968053e18e7954b4a277f03b
Silevran <Meliroon@gmail.com>
2021-06-12 22:36:29 -0700

    Fixing the ability icons.

--- Assets/_/Scripts/UI/AbilityIconToggle.cs
+++ Assets/_/Scripts/UI/AbilityIconToggle.cs
@@ -1,14 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Assets.__.Scripts.PlayerScripts;
+using Assets.__.Scripts;
 
 public class AbilityIconToggle : MonoBehaviour
 {
     [SerializeField] private PlayerEffect ability;
     [SerializeField] private GameObject activeAbilityImage;
     [SerializeField] private GameObject inactiveAbilityImage;
+
+    private GameModel gameModel;
 
     private void Awake()
     {
@@ -21,9 +21,17 @@ public class AbilityIconToggle : MonoBehaviour
         {
             Debug.LogWarning("inactiveAbilityImage in AbilityIconToggle not set in inspector under Ability Icons Panel");
         }
+
+        gameModel = GameModel.GetInstance();
     }
 
-
+    private void Update()
+    {
+        if(gameModel.ActivePlayerEffects.ContainsKey(ability))
+        {
+            SetActive(gameModel.ActivePlayerEffects[ability] > 0);
+        }
+    }
 
     public void SetActive(bool isActive)
     {
