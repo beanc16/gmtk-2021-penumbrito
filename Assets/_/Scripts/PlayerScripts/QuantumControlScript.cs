80055c65732653f599098ea60214b0838d58934c
Silevran <Meliroon@gmail.com>
2021-06-12 19:29:38 -0700

    Fixing some issues with dieing.

--- Assets/_/Scripts/PlayerScripts/QuantumControlScript.cs
+++ Assets/_/Scripts/PlayerScripts/QuantumControlScript.cs
@@ -149,6 +149,7 @@ public class QuantumControlScript : MonoBehaviour
         if (gameModel.ReloadScene)
         {
             SceneHandler.RestartCurrentScene();
+            return;
         }
 
         if (gameModel.CountInWinZone == this.cachedControllablePlayers.Length)
