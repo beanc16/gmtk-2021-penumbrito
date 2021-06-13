9e30351a475955509e0f2787afd1721e9120ca85
Silevran <Meliroon@gmail.com>
2021-06-12 19:03:53 -0700

    More merge conflict fixes.

--- Assets/_/Scripts/EntangleView/EntanglePanel.cs
+++ Assets/_/Scripts/EntangleView/EntanglePanel.cs
@@ -15,37 +15,6 @@ namespace Assets.__.Scripts.EntangleView
         {
             this.selectButton = this.GetComponent<Button>();
             this.imagePanel = this.GetComponentInChildren<Image>();
-<<<<<<< HEAD
-
-            this.selectButton.onClick.AddListener(this.OnClickedPanel);
-        }
-
-        private void OnDestroy()
-        {
-            this.selectButton.onClick.RemoveAllListeners();
-        }
-
-        private void OnClickedPanel()
-        {
-            GameObject quantumController = GameObject.FindGameObjectsWithTag("QuantumController")[0];
-            QuantumControlScript quantumControlScript = quantumController.GetComponent<QuantumControlScript>();
-            if (!quantumControlScript.CanUpdatePanel())
-            {
-                return;
-            }
-
-            var gameModel = GameModel.GetInstance();
-            gameModel.ActivePanels[this.panelNumber] = !gameModel.ActivePanels[this.panelNumber];
-
-            gameModel.UpdatePlayerEffect(this.panelNumber, gameModel.ActivePanels[this.panelNumber]);
-
-            //imagePanel.gameObject.SetActive(gameModel.ActivePanels[this.panelNumber]);
-            this.SetLightStates(gameModel.ActivePanels[this.panelNumber]);
-            //Inform systems that need it of who is active and who is not.
-
-            gameModel.UpdateActivePanels();
-=======
->>>>>>> 86d0e030d9d2b1c13a517e267fd56038c2c997f5
         }
 
         public void Setup(int panelNumber)
